8f6468ab930bfc2d6865e88d20d8e33a871ef536
EricBoix <eric.boix@insa-lyon.fr>
2026-05-29 14:38:28 +0200

    More Dockerfile debug

--- DockerContext/entrypoint.py
+++ DockerContext/entrypoint.py
@@ -5,9 +5,9 @@ import sys
 import subprocess
 
 if __name__ == "__main__":
-    this_script_dir = pathlib.Path(__file__).parent.resolve()
-    converter_dir = os.path.join(this_script_dir, "Convert")
-    convert_script_file = os.path.join(converter_dir, "main.py")
+    # this_script_dir = pathlib.Path(__file__).parent.resolve()
+    # converter_dir = os.path.join(this_script_dir, "..", "Convert")
+    convert_script_file = "main.py"
     if not os.path.exists(convert_script_file):
         print(f"Converter script ({convert_script_file}) not found. Exiting.")
         sys.exit()
